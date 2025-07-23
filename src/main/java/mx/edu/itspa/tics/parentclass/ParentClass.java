310cf8e8b7bca6642bd4e334247d77f16e453383
Gama <gamacontacto56@gmail.com>
2025-07-22 18:04:11 -0600

    practica 22/7/2025

---
+++ src/main/java/mx/edu/itspa/tics/parentclass/ParentClass.java
@@ -0,0 +1,12 @@
+
+package mx.edu.itspa.tics.parentclass;
+
+public class ParentClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        
+    }
+}
